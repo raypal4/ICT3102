388a350144775c38424a029a1567541588ac1ec4
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-09-15 19:23:54 +0800

    extractbeacon endpoint roughly done

--- HAWCS/app.py
+++ HAWCS/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # insert your IP here
 ip_address = "3.141.36.97"
+# ip_address = "localhost"
 
 @app.route('/')
 def index():
@@ -18,28 +19,30 @@ def ping_server_0():
 	start_timestamp = end_timestamp - 10
 
 	# temp_string = "http://"+ip_address+":5000/extractbeacon?staff_id=0&start_time="+str(start_timestamp)+"&end_time="+str(end_timestamp)
-	temp_string = "http://"+ip_address+"/test-route"
+	temp_string = "http://"+ip_address+"/extractbeacon?staff_id=0&start_time="+str(start_timestamp)+"&end_time="+str(end_timestamp)
 
 	r0 = requests.get(temp_string)
 	print(r0.text)
 	return (r0.text)
 
-# def ping_server_2():
-# 	end_timestamp = int(time.time())
-# 	start_timestamp = end_timestamp - 10
+def ping_server_2():
+	end_timestamp = int(time.time())
+	start_timestamp = end_timestamp - 10
 
-# 	temp_string = "http://"+ip_address+":5000/extractbeacon?staff_id=2&start_time="+str(start_timestamp)+"&end_time="+str(end_timestamp)
-# 	r2 = requests.get(temp_string)
-# 	print(r2.text)
-# 	return (r2.text)
+	# temp_string = "http://"+ip_address+":5000/extractbeacon?staff_id=2&start_time="+str(start_timestamp)+"&end_time="+str(end_timestamp)
+	temp_string = "http://"+ip_address+"/extractbeacon?staff_id=2&start_time="+str(start_timestamp)+"&end_time="+str(end_timestamp)
+
+	r2 = requests.get(temp_string)
+	print(r2.text)
+	return (r2.text)
 
 
 sched_0 = BackgroundScheduler(daemon=True)
 sched_0.add_job(ping_server_0,'interval',seconds=10)
 sched_0.start()
 
-# sched_2 = BackgroundScheduler(daemon=True)
-# sched_2.add_job(ping_server_2,'interval',seconds=10)
-# sched_2.start()
+sched_2 = BackgroundScheduler(daemon=True)
+sched_2.add_job(ping_server_2,'interval',seconds=10)
+sched_2.start()
 
 app.run(debug=True, host='0.0.0.0')
---
+++ Server/routes/BeaconRoutes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+from datasource.StaffGateway import StaffGateway
+
+# Initialization
+router = Blueprint("BeaconRoutes", __name__)
+
+@router.route("/extractbeacon", methods=['GET', 'POST'])
+def extractbeacon():
+    staff_id = int(request.args.get('staff_id'))
+    start_time = int(request.args.get('start_time'))
+    end_time = int(request.args.get('end_time'))
+
+    locations = StaffGateway().retrieve_staff_location(staff_id, start_time, end_time)
+    return jsonify({
+        "staff_id": staff_id,
+        "locations": locations
+    })
