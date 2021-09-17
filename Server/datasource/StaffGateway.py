ff663644016b3a347985ff606d5d94aa75e8ea40
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-09-17 17:50:01 +0800

    working on newbeacondetect
    newBeaconDetect()
    
    BeaconGateway Class creation

--- Server/datasource/StaffGateway.py
+++ Server/datasource/StaffGateway.py
@@ -4,6 +4,7 @@ class StaffGateway:
     # Static
     __StaffCollection = Database.db["staff"]
 
+    # retrieve all locations detected within the timestamp
     def retrieve_staff_location(self, id, start_time, end_time):
         collection = StaffGateway.__StaffCollection
         allStaffVisitedLocations = collection.find_one({"staff_id": id})["location"]
@@ -12,5 +13,9 @@ class StaffGateway:
         for item in allStaffVisitedLocations:
             if start_time <= int(item["timestamp"]) <= end_time:
                 temp.append(item)
-
         return temp
+
+    # add new location based on user and detected beacon to the db
+    def add_new_staff_location(self, user_address, level, location):
+        print(user_address, level, location)
+        pass
--- Server/routes/BeaconRoutes.py
+++ Server/routes/BeaconRoutes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, jsonify, request
 from datasource.StaffGateway import StaffGateway
+from datasource.BeaconGateway import BeaconGateway
 
 # Initialization
 router = Blueprint("BeaconRoutes", __name__)
 
 @router.route("/extractbeacon", methods=['GET', 'POST'])
-def extractbeacon():
+def extractBeacon():
     staff_id = int(request.args.get('staff_id'))
     start_time = int(request.args.get('start_time'))
     end_time = int(request.args.get('end_time'))
@@ -14,3 +15,15 @@ def extractbeacon():
     return jsonify({
         "location": locations
     })
+
+@router.route("/newbeacondetect", methods=['GET','POST'])
+def newBeaconDetect():
+    # user_address = request.args.get('user_address')
+    # beacon_address = request.args.get('beacon_address')
+    beacon_details = BeaconGateway().get_location_details("C2A628384B08")
+    print(beacon_details)
+
+    # add new beacon detection details to db
+    StaffGateway().add_new_staff_location(0, beacon_details["beaconLevel"], beacon_details["beaconLocation"])
+    return f"New Beacon Detection Added" 
+
