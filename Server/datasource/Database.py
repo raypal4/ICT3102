24c411daac8b03fbc9dead70494a1ba07eae078d
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-09-18 14:08:44 +0800

    Update Database.py

--- Server/datasource/Database.py
+++ Server/datasource/Database.py
@@ -2,5 +2,5 @@ import pymongo
 
 class Database:
     client = pymongo.MongoClient(
-        "mongodb+srv://ray:raynoldtan@cluster0.xhxln.mongodb.net/ICT3102?retryWrites=true&w=majority")
+        "mongodb+srv://ray:3JKQm3bliqZpCDP4@cluster0.xhxln.mongodb.net/ICT3102?retryWrites=true&w=majority")
     db = client["ICT3102"]
