16f61e06a7aa3ffc88336d082a7f55fd63803b2b
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-10-18 16:19:34 +0800

    docker compose

--- Server/routes/BeaconRoutes.py
+++ Server/routes/BeaconRoutes.py
@@ -7,10 +7,8 @@ import random
 # Initialization
 router = Blueprint("BeaconRoutes", __name__)
 
-# Globals
-ThreadCounter = 0
-ThreadController = ThreadController()
 BeaconControl = BeaconControl()
+
 
 @router.route("/extractbeacon", methods=['GET', 'POST'])
 def extractBeacon():
@@ -18,45 +16,17 @@ def extractBeacon():
     start_time = int(request.args.get('start_time'))
     end_time = int(request.args.get('end_time'))
 
-    locations = BeaconControl.retrieve_staff_location(staff_id, start_time, end_time)
+    locations = BeaconControl.retrieve_staff_location(
+        staff_id, start_time, end_time)
     return jsonify({
         "location": locations
     })
 
-@router.route("/newbeacondetect", methods=['GET','POST'])
+
+@router.route("/newbeacondetect", methods=['GET', 'POST'])
 def newBeaconDetect():
     user_address = int(request.args.get('user_address'))
     beacon_address = request.args.get('beacon_address')
     rssi = int(request.args.get('rssi'))
-
-    # Temp Random addtion to mock mobile device request
-    # x = random.sample(set([0,2]), 1)
-    # y = random.choice(["C2A628384B08","EB73768336D9"])
-    # BeaconControl.new_beacon_detect(x[0], y, -20)
-    
     BeaconControl.new_beacon_detect(user_address, beacon_address, rssi)
-
-    return f"New Beacon Detection Added" 
-
-### Test routes for beacon creation ###
-# Start Beacon detected every 10 seconds 
-@router.route("/starttestbeaconcreation")
-def start_test_thread():
-    global ThreadCounter, ThreadController
-
-    if ThreadCounter <= 0:
-        instance = Thread(1, "testBeaconDetection", ThreadController)
-        instance.start()
-        ThreadCounter += 1
-        return "adding in beacons every 10 seconds"
-    else:
-        return "thread to creation detections already running"
-
-# Stop Beacon detection
-@router.route("/stoptestbeaconcreation")
-def stop_test_thread():
-    global ThreadCounter, ThreadController
-    ThreadController.stop()
-    ThreadCounter -= 1
-    return "thread stopped"
-
+    return f"New Beacon Detection Added"
