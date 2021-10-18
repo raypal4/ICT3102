52b55d93afd63589c19cd2c28d866ae75c6daa58
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-10-18 14:34:22 +0800

    waitress added

--- Server/app.py
+++ Server/app.py
@@ -3,6 +3,7 @@ from routes import (
     BeaconRoutes,
 )
 from domain.BeaconControl import BeaconControl
+from waitress import serve
 
 app = Flask(__name__)
 # Configuration
@@ -10,17 +11,18 @@ app = Flask(
     __name__,
     static_url_path="/",
     static_folder="static",
-    template_folder= "templates",
+    template_folder="templates",
 )
+
 
 @app.route("/")
 def index():
     data = BeaconControl().retrieve_all_staff()
-    return render_template("index.html" , data=data)
+    return render_template("index.html", data=data)
 
 
 app.register_blueprint(BeaconRoutes.router)
 
 if __name__ == "__main__":
     # NGINX ver TODO remove port 80
-    app.run(host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
