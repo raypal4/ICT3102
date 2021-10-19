cbdf5358ee1b786d327a172a8c9d0c27f0a11c2c
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-10-19 22:10:07 +0800

    THIS IS THE CORRECT SETUP FOR MONGODB

--- Server/datasource/Database.py
+++ Server/datasource/Database.py
@@ -2,6 +2,5 @@ import pymongo
 from pymongo import ssl_support
 
 class Database:
-    client = pymongo.MongoClient(
-        "mongodb+srv://ray:password1234@cluster0.xhxln.mongodb.net/ICT3102?retryWrites=true&w=majority", ssl_cert_reqs=ssl_support.CERT_NONE)
+    client = pymongo.MongoClient("mongodb://172.17.0.1:27017")
     db = client["ICT3102"]
