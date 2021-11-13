9acd4cb52d4f191d26bfb4ece5924d516df0f217
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-11-13 23:29:06 +0800

    cb

--- Server/routes/BeaconRoutes.py
+++ Server/routes/BeaconRoutes.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, jsonify, request
-from domain.Thread import Thread
-from domain.ThreadController import ThreadController
 from domain.BeaconControl import BeaconControl
-import random
+import pybreaker
 
 # Initialization
 router = Blueprint("BeaconRoutes", __name__)
 
 BeaconControl = BeaconControl()
+route_breaker = pybreaker.CircuitBreaker(fail_max=5, reset_timeout=30)
 
 @router.route("/extractbeacon", methods=['GET', 'POST'])
+@route_breaker
 def extractBeacon():
     staff_id = int(request.args.get('staff_id'))
     start_time = int(request.args.get('start_time'))
@@ -22,6 +22,7 @@ def extractBeacon():
     })
 
 @router.route("/newbeacondetect", methods=['GET', 'POST'])
+@route_breaker
 def newBeaconDetect():
     user_address = int(request.args.get('user_address'))
     beacon_address = request.args.get('beacon_address')
@@ -30,6 +31,7 @@ def newBeaconDetect():
     return f"New Beacon Detection Added"
 
 @router.route("/retrieveformonitoring", methods=['GET', 'POST'])
+@route_breaker
 def retrieveForMonitoring():
     data = BeaconControl.retrieve_all_staff()
     return jsonify({
