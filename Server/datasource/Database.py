96b3c1ff8c65affbc36550dc38fc8853413453d2
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-11-20 20:55:52 +0800

    Update Database.py

--- Server/datasource/Database.py
+++ Server/datasource/Database.py
@@ -3,8 +3,8 @@ from pymongo import ssl_support
 import os
 
 class Database:
-    # client = pymongo.MongoClient(os.environ["MONGOSCONNECTIONSTRING"])
-    client = pymongo.MongoClient("mongodb://localhost:27017")
+    client = pymongo.MongoClient(os.environ["MONGOSCONNECTIONSTRING"])
+    # client = pymongo.MongoClient("mongodb://localhost:27017")
 
     db = client["ICT3102"]
     
