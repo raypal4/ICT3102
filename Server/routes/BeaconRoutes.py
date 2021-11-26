0466acec6651b7e87fd3df25f384e4cca7b68d00
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-11-26 19:25:07 +0800

    Update BeaconRoutes.py

--- Server/routes/BeaconRoutes.py
+++ Server/routes/BeaconRoutes.py
@@ -10,32 +10,32 @@ BeaconControl = BeaconControl()
 
 @router.route("/extractbeacon", methods=['GET', 'POST'])
 def extractBeacon():
-    # try:
-    staff_id = int(request.args.get('staff_id'))
-    start_time = int(request.args.get('start_time'))
-    end_time = int(request.args.get('end_time'))
-    
-    locations = BeaconControl.retrieve_staff_location(staff_id, start_time, end_time)
-    return jsonify({
-        "location": locations
-    })
-    # except:
-    #     return jsonify({"location": "Invalid get params"})
+    try:
+        staff_id = int(request.args.get('staff_id'))
+        start_time = int(request.args.get('start_time'))
+        end_time = int(request.args.get('end_time'))
+        
+        locations = BeaconControl.retrieve_staff_location(staff_id, start_time, end_time)
+        return jsonify({
+            "location": locations
+        })
+    except:
+        return jsonify({"location": "Invalid get params"})
 
 
 @router.route("/newbeacondetect", methods=['GET', 'POST'])
 def newBeaconDetect():
-    # try:
-    staff_id = int(request.args.get('staff_id'))
-    beacon_address = request.args.get('beacon_address')
-    rssi = int(request.args.get('rssi'))
-    res = BeaconControl.new_beacon_detect(staff_id, beacon_address, rssi)
-    if res != False:
-        return f"New Beacon Detection Added"
-    else:
-        return f"Invalid Beacon"
-    # except:
-    #     return jsonify({"location": "Invalid get params"})
+    try:
+        staff_id = int(request.args.get('staff_id'))
+        beacon_address = request.args.get('beacon_address')
+        rssi = int(request.args.get('rssi'))
+        res = BeaconControl.new_beacon_detect(staff_id, beacon_address, rssi)
+        if res != False:
+            return f"New Beacon Detection Added"
+        else:
+            return f"Invalid Beacon"
+    except:
+        return jsonify({"location": "Invalid get params"})
 
 @router.route("/retrieveformonitoring", methods=['GET', 'POST'])
 def retrieveForMonitoring():
